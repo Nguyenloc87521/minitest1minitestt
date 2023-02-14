63f6b588f1021ca36f4e94b5a425e9a8c66e2e63
Nguyenloc87521 <117912099+Nguyenloc87521@users.noreply.github.com>
2023-02-14 22:14:24 +0700

    Update lc75.java

--- src/main/java/lc75.java
+++ src/main/java/lc75.java
@@ -1,2 +1,19 @@
 package PACKAGE_NAME;public class lc75 {
+      public void sortColors(int[] nums) {
+        int idx=0;
+        int idx1 = nums.length -1;
+        for (int i = 0; i <= idx1; i++) {
+            if(nums[i]==0 && i != idx){
+                int tmp = nums[idx];
+                nums[idx++]=nums[i];
+                nums[i]=tmp;
+            } else if (nums[i] == 2 && i != idx1) {
+                int tmp = nums[idx1];
+                nums[idx1--] = nums[i];
+                nums[i] = tmp;
+            } else {
+                i++;
+            }
+        }
+    }
 }
