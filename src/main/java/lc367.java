93b06a3549a472ae748cd5355a9156b9fe6c4786
Nguyenloc87521 <117912099+Nguyenloc87521@users.noreply.github.com>
2023-02-14 22:13:28 +0700

    Update lc367.java

--- src/main/java/lc367.java
+++ src/main/java/lc367.java
@@ -1,2 +1,14 @@
 package PACKAGE_NAME;public class lc367 {
+      public boolean isPerfectSquare(int num) {
+
+        if(num < 0){
+            return false;
+        }
+        for (int i = 0;   i<=num/i; i++) {
+            if (i*i==num){
+                return true;
+            }
+        }
+        return false;
+    }
 }
